d056fd542c7ee295428bb8b9bca6868284bf578d
Kwadwo Agyapon-Ntra <kayoghonline@gmail.com>
2020-06-12 08:48:05 +0000

    Made swapi.co to swapi.dev change in test file

--- script2.test.js
+++ script2.test.js
@@ -32,7 +32,7 @@ it('getPeople returns count and results', () => {
   expect.assertions(4)
   return swapi.getPeoplePromise(mockFetch).then(data => {
     expect(mockFetch.mock.calls.length).toBe(1);
-    expect(mockFetch).toBeCalledWith('https://swapi.co/api/people');
+    expect(mockFetch).toBeCalledWith('https://swapi.dev/api/people');
     expect(data.count).toEqual(87);
     expect(data.results.length).toBeGreaterThan(5);
   })
